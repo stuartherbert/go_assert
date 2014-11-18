ffbf20b1bb19310056a4f7241fb88cfec7f0ae6e
Stuart Herbert <stuart@stuartherbert.com>
2014-11-18 15:42:22 +0000

    New: asserts work outside of the "go test" environment

--- README.md
+++ README.md
@@ -1,45 +1,26 @@
 # Assert (c) Blake Mizerany and Keith Rarick -- MIT LICENCE
 
-## Assertions for Go tests
+## Assertions for Go packages
+
+Forked from https://github.com/bmizerany/assert and modified to work outside of the 'go test' environment.
+
+If you're looking for asserts to use in your tests, you should definitely use their package rather than my fork!
 
 ## Install
 
-    $ go get github.com/bmizerany/assert
+    $ go get github.com/stuartherbert/go_assert
 
 ## Use
 
-**point.go**
-
-    package point
-
-    type Point struct {
-        x, y int
-    }
-
-**point_test.go**
-
-
-    package point
-
-    import (
-        "testing"
-        "github.com/bmizerany/assert"
+    import(
+        "github.com/stuartherbert/go_assert"
     )
 
-    func TestAsserts(t *testing.T) {
-        p1 := Point{1, 1}
-        p2 := Point{2, 1}
+    func DoSomething(
+        expected := "foo"
+        actual := "bar"
 
-        assert.Equal(t, p1, p2)
-    }
-
-**output**
-    $ go test
-     --- FAIL: TestAsserts (0.00 seconds)
-	 assert.go:15: /Users/flavio.barbosa/dev/stewie/src/point_test.go:12
-         assert.go:24: ! X: 1 != 2
-	 FAIL
+        assert.Equal(expected, actual)
+    )
 
 ## Docs
-
-    http://github.com/bmizerany/assert
--- assert.go
+++ assert.go
@@ -1,76 +1,88 @@
+// Assert (c) Blake Mizerany and Keith Rarick -- MIT LICENCE
+//
+// Forked and modified to run outside of the 'go test' environment
 package assert
+
 // Testing helpers for doozer.
 
 import (
-	"github.com/kr/pretty"
-	"reflect"
-	"testing"
-	"runtime"
-	"fmt"
+    "fmt"
+    "github.com/kr/pretty"
+    "os"
+    "reflect"
+    "runtime"
 )
 
-func assert(t *testing.T, result bool, f func(), cd int) {
-	if !result {
-		_, file, line, _ := runtime.Caller(cd + 1)
-		t.Errorf("%s:%d", file, line)
-		f()
-		t.FailNow()
-	}
+func FailNow() {
+    os.Exit(1)
 }
 
-func equal(t *testing.T, exp, got interface{}, cd int, args ...interface{}) {
-	fn := func() {
-		for _, desc := range pretty.Diff(exp, got) {
-			t.Error("!", desc)
-		}
-		if len(args) > 0 {
-			t.Error("!", " -", fmt.Sprint(args...))
-		}
-	}
-	result := reflect.DeepEqual(exp, got)
-	assert(t, result, fn, cd+1)
+func Error(args ...interface{}) {
+    fmt.Println(args...)
 }
 
-func tt(t *testing.T, result bool, cd int, args ...interface{}) {
-	fn := func() {
-		t.Errorf("!  Failure")
-		if len(args) > 0 {
-			t.Error("!", " -", fmt.Sprint(args...))
-		}
-	}
-	assert(t, result, fn, cd+1)
+func assert(result bool, f func(), cd int) {
+    if !result {
+        _, file, line, _ := runtime.Caller(cd + 1)
+        fmt.Printf("%s:%d", file, line)
+        f()
+        FailNow()
+    }
 }
 
-func T(t *testing.T, result bool, args ...interface{}) {
-	tt(t, result, 1, args...)
+func equal(exp, got interface{}, cd int, args ...interface{}) {
+    fn := func() {
+        for _, desc := range pretty.Diff(exp, got) {
+            Error("!", desc)
+        }
+        if len(args) > 0 {
+            Error("!", " -", fmt.Sprint(args...))
+        }
+    }
+    result := reflect.DeepEqual(exp, got)
+    assert(result, fn, cd+1)
 }
 
-func Tf(t *testing.T, result bool, format string, args ...interface{}) {
-	tt(t, result, 1, fmt.Sprintf(format, args...))
+func tt(result bool, cd int, args ...interface{}) {
+    fn := func() {
+        Error("!  Failure")
+        if len(args) > 0 {
+            Error("!", " -", fmt.Sprint(args...))
+        }
+    }
+    assert(result, fn, cd+1)
 }
 
-func Equal(t *testing.T, exp, got interface{}, args ...interface{}) {
-	equal(t, exp, got, 1, args...)
+func T(result bool, args ...interface{}) {
+    tt(result, 1, args...)
 }
 
-func Equalf(t *testing.T, exp, got interface{}, format string, args ...interface{}) {
-	equal(t, exp, got, 1, fmt.Sprintf(format, args...))
+func Tf(result bool, format string, args ...interface{}) {
+    tt(result, 1, fmt.Sprintf(format, args...))
 }
 
-func NotEqual(t *testing.T, exp, got interface{}, args ...interface{}) {
-	fn := func() {
-		t.Errorf("!  Unexpected: <%#v>", exp)
-		if len(args) > 0 {
-			t.Error("!", " -", fmt.Sprint(args...))
-		}
-	}
-	result := !reflect.DeepEqual(exp, got)
-	assert(t, result, fn, 1)
+func Equal(exp, got interface{}, args ...interface{}) {
+    equal(exp, got, 1, args...)
 }
 
-func Panic(t *testing.T, err interface{}, fn func()) {
-	defer func() {
-		equal(t, err, recover(), 3)
-	}()
-	fn()
+func Equalf(exp, got interface{}, format string, args ...interface{}) {
+    equal(exp, got, 1, fmt.Sprintf(format, args...))
+}
+
+func NotEqual(exp, got interface{}, args ...interface{}) {
+    fn := func() {
+        Error("!  Unexpected: <%#v>", exp)
+        if len(args) > 0 {
+            Error("!", " -", fmt.Sprint(args...))
+        }
+    }
+    result := !reflect.DeepEqual(exp, got)
+    assert(result, fn, 1)
+}
+
+func Panic(err interface{}, fn func()) {
+    defer func() {
+        equal(err, recover(), 3)
+    }()
+    fn()
 }
